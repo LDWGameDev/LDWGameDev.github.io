e5288b1a2e4fa7dfa9fff5dbbdd2185e10a3eb09
Do Long <ldwork.gamedev01@gmail.com>
2025-02-08 12:23:57 +0700

    - Deployed for first portfolio version with 2 pages and 5 projects.

--- content/page/project.md
+++
@@ -1,6 +0,0 @@
----
-Title: "LDWORK"
-layout: "project_detail"
-url: "/project/"
-Subtitle: "Unreal Engine Gameplay Developer"
----
--- tools/convert_xlsx_to_txt.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: Run the Python script to convert xlsx to txt
-python convert_projects_xlsx_to_txt.py
-
-:: Copy the content of output.txt to the clipboard
-powershell -command "Get-Content output.txt | Set-Clipboard"
-
